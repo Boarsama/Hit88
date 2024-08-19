eb5a50d7adc7189c4967ff28116d29d377497372
Boarsama <165765445+Boarsama@users.noreply.github.com>
2024-08-19 11:29:37 +0700

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,18 +1,13 @@
-document.getElementById('spinBtn').addEventListener('click', function() {
-    const wheel = document.getElementById('wheel');
-    // Quay vòng quay
-    wheel.classList.add('spinning');
+document.getElementById('nhanNgayBtn').addEventListener('click', function() {
+    // Hiển thị spinner và ẩn button
+    document.getElementById('loading').style.display = 'block';
+    document.getElementById('nhanNgayBtn').style.display = 'none';
 
+    // Độ trễ 1s để tạo hiệu ứng loading rồi chuyển hướng đến trang đích
     setTimeout(function() {
-        // Hiển thị pop-up sau khi quay xong
-        document.getElementById('rewardPopup').style.display = 'flex';
-    }, 13000); // 3 giây cho vòng quay
-});
-
-document.getElementById('closePopup').addEventListener('click', function() {
-    document.getElementById('rewardPopup').style.display = 'none';
-});
-
-document.getElementById('claimReward').addEventListener('click', function() {
-    window.open('https://hit88.trangchu.online', '_blank');
+        window.open('https://hit88.trangchu.online', '_blank');
+        // Ẩn spinner và hiện lại button khi hoàn thành
+        document.getElementById('loading').style.display = 'none';
+        document.getElementById('nhanNgayBtn').style.display = 'block';
+    }, 1000);
 });
